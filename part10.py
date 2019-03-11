4e0d54a93dca126580d4c6f106808c5d1b538647
gykwon <bonecollect2@gmail.com>
2019-03-11 12:43:01 +0900

    part 10 - try except 추가

--- part10.py
+++ part10.py
@@ -1,25 +1,34 @@
 my_list = list()
-idx = 0
+idx = int()
+num = int()
 def print_element(arg, index):
-    print("{0}번째 데이터가 추출 됩니다.".format(idx))
     print(arg.pop(int(idx)-1))
 
 
 
+try:
+    print("몇개의 숫자를 입력하시겠어요??")
+    num = input()
+    int(num)
+
+    print("Enter로 구분하여 숫자를 입력하세요")
+
+    for i in range(int(num)):
+        input_num = input()
+        my_list.append(int(input_num))
 
 
-print("몇개의 숫자를 입력하시겠어요??")
-num = input()
+    print("입력한 데이터는 {0} 입니다.".format(my_list))
+    print("몇번째 데이터를 추출할까요??")
+    idx = input()
 
-print("Enter로 구분하여 숫자를 입력하세요")
-
-for i in range(int(num)):
-    input_num = input()
-    my_list.append(int(input_num))
+    print_element(my_list,idx)
 
 
-print("입력한 데이터는 {0} 입니다.".format(my_list))
-print("몇번째 데이터를 추출할까요??")
-idx = input()
 
-print_element(my_list,idx)
+except ValueError:
+    print("Error : 숫자를 입력하세요")
+
+
+except IndexError:
+    print("index를 초과 하였습니다.")
